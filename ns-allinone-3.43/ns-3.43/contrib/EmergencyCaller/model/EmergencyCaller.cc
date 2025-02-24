88002af2fe1384371f039de199ab597f7ee9464c
Shamanth Moodalbyle <shamanth.moodalbyle@student.manchester.ac.uk>
2025-02-24 10:27:24 +0000

    Changed mobility from random2walk to random waypoint

--- ns-allinone-3.43/ns-3.43/contrib/EmergencyCaller/model/EmergencyCaller.cc
+++ ns-allinone-3.43/ns-3.43/contrib/EmergencyCaller/model/EmergencyCaller.cc
@@ -14,15 +14,23 @@ void EmergencyCaller::add_nodes(std::vector<ns3::Vector3D>& node_coordinates)
     }
 
     caller_mobility.SetPositionAllocator(list_position_alloc);
-    caller_mobility.SetMobilityModel ("ns3::RandomWalk2dMobilityModel",
-                             "Bounds", RectangleValue (Rectangle (0, 100, 0, 100)));    
+
+      // Set up a RandomRectanglePositionAllocator for destination selection
+    Ptr<RandomRectanglePositionAllocator> randomAlloc = CreateObject<RandomRectanglePositionAllocator> ();
+    randomAlloc->SetAttribute("X", StringValue("ns3::UniformRandomVariable[Min=0.0|Max=100.0]"));
+    randomAlloc->SetAttribute("Y", StringValue("ns3::UniformRandomVariable[Min=0.0|Max=100.0]"));
+
+    caller_mobility.SetMobilityModel("ns3::RandomWaypointMobilityModel",
+                                     "Speed", StringValue("ns3::ConstantRandomVariable[Constant=10.0]"),
+                                      // "Pause", StringValue("ns3::ConstantRandomVariable[Constant=50.0]"),
+                                     "PositionAllocator", PointerValue(randomAlloc));    
     caller_mobility.Install(container);
 
 
     for (size_t i = 0; i < node_coordinates.size(); ++i)
     {
         Ptr<Node> node = container.Get(i);
-        Ptr<RandomWalk2dMobilityModel > mobility = node->GetObject<RandomWalk2dMobilityModel >();
+        Ptr<RandomWaypointMobilityModel > mobility = node->GetObject<RandomWaypointMobilityModel>();
         // node->AggregateObject(mobility);
         mobility->SetPosition(node_coordinates[i]);
     }
